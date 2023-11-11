3c8abd349e9bd8ee88fbcc13fb7383ae41590d8c
Jose Perez <jmperezvzla@gmail.com>
2023-11-11 17:12:07 -0400

    Active filterResults in form Add transition animation with post-hidden

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,8 +14,30 @@ import { PostsService } from '../posts.service';
 export class HomeComponent {
   postsList: Postlist[] = [];
   postsService: PostsService = inject(PostsService);
+  filteredPostsList: Postlist[] = [];
+  postsHidden: Boolean = false;
 
   constructor() {
     this.postsList = this.postsService.getAllPosts();
+    this.filteredPostsList = this.postsList;
+  }
+
+  filterResults(text: string, $event: Event) {
+    $event.preventDefault();
+    this.postsHidden = true;
+
+    let temp: Postlist[] = [];
+    if (!text) {
+      temp = this.postsList;
+    } else {
+      temp = this.postsList.filter((postList) =>
+        postList?.city.toLowerCase().includes(text.toLowerCase()),
+      );
+    }
+
+    setTimeout(() => {
+      this.postsHidden = false;
+      this.filteredPostsList = temp;
+    }, 500);
   }
 }
