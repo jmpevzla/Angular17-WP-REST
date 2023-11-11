f8a48a76fc48321812838992d546dc83089df5cc
Jose Perez <jmperezvzla@gmail.com>
2023-11-11 12:45:07 -0400

    Add form details for apply to a house

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,13 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { PostsService } from '../posts.service';
 import { Postlist } from '../postlist';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
@@ -15,9 +16,22 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   postsService = inject(PostsService);
   postList: Postlist | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
 
   constructor() {
     const postId = Number(this.route.snapshot.params['id']);
     this.postList = this.postsService.getPostById(postId);
+  }
+
+  submitApplication() {
+    this.postsService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
   }
 }
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -120,4 +120,10 @@ export class PostsService {
   getPostById(id: number): Postlist | undefined {
     return this.postsList.find((post) => post.id === id);
   }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`,
+    );
+  }
 }
